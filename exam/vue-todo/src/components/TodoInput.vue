b5b3779b4d526eb9cec49ded123d187fc27deb4c
김하은 <wha02068@gmail.com>
2022-08-29 13:31:11 +0900

    아무 입력 안햇을 때 모달 뜨는 거 까지

--- exam/vue-todo/src/components/TodoInput.vue
+++ exam/vue-todo/src/components/TodoInput.vue
@@ -5,14 +5,41 @@
       <i class="addBtn fas fa-plus" aria-hidden="true"/>
     </span>
     <!-- <button v-on:click="addTodo">추가</button> -->
+    <modal v-if="showModal" @close="showModal = false">
+      <h3>경고</h3>
+      <span slot="footer" @click="showModal = false">
+        할 일을 입력하세요
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
+
+    <Teleport to="body">
+    <!-- use the modal component, pass in the prop -->
+    <modal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>경고</h3>
+      </template>
+      <template #footer >
+        <h3>할 일을 입력하세요</h3>
+        <i class="closeModalBtn fas fa-times" aria-hidden="true" @click="showModal = false"></i>
+      </template>
+    </modal>
+  </Teleport>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
+  components: {
+    Modal: Modal
+  },
+  props: ['propsdata'],
   data() {
     return {
-      newTodoItem: ''
+      newTodoItem: '',
+      showModal: false
     }
   },
   methods: {
@@ -20,15 +47,19 @@ export default {
       //console.log(this.newTodoItem)
       if (this.newTodoItem !== "") {
         var value = this.newTodoItem && this.newTodoItem.trim();
-        localStorage.setItem(this.newTodoItem, this.newTodoItem);
+        //localStorage.setItem(this.newTodoItem, this.newTodoItem);
+        this.$emit('addTodo', value);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+        console.log(this.showModal);
+        console.log("empty");
       }
     },
     clearInput() {
       this.newTodoItem = '';
     }
   }
-
 }
 </script>
 
